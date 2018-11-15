6bc436a6d6d5b2c59b63f755c24e849adf029d65
Janne Holopainen <janne.holopainen@reaktor.com>
2018-11-16 00:01:46 +0200

    Clean data & change definiton for family

--- model/wellbeing.py
+++ model/wellbeing.py
@@ -23,14 +23,26 @@ model {
 """
 paavo_df = pd.read_csv('data/paavo_9_koko.csv', sep=';')
 
-paavo_sub = paavo_df[paavo_df['Young single persons, 2016 (TE)'] != "."] # Exclude empty observations
+# Drop no observations from the DF
+paavo_sub = paavo_df[(paavo_df['Young single persons, 2016 (TE)'] != ".") & (paavo_df["Young single persons, 2016 (TE)"] != "..")] # Exclude empty observations
 paavo_sub = paavo_sub[paavo_sub['Young couples without children, 2016 (TE)'] != "."]
-# TODO paavo_sub still contains ".." observations.
 
-#paavo_sub['nchildless'] = paavo_sub['Young single persons, 2016 (TE)'].astype(int) + paavo_sub['Young couples without children, 2016 (TE)'].astype(int)
-print(paavo_sub['nchildless'][:20])
+
+# Drop whole Finland
+paavo_sub = paavo_sub.iloc[1:, :]
+
+# Include nchildless
+paavo_sub['nchildless_young'] = paavo_sub['Young single persons, 2016 (TE)'].astype(int) + paavo_sub['Young couples without children, 2016 (TE)'].astype(int)
+paavo_sub['nchildless'] = paavo_sub['Adult households, 2016 (TE)'].astype(int) + paavo_sub['Pensioner households, 2016 (TE)']
+paavo_sub["nfamilies"] = paavo_sub["Households with children, 2016 (TE)"].astype(int)
+
+# IF one wants to work only with capital area.
+# Contains Helsinki, Vantaa, Espoo + Some neighbours
+paavo_sub = paavo_sub.loc[paavo_sub["Postal code area"].apply(lambda x: x[:2] in ["00", "01", "02"])]
+
+
 '''
-N = paavo_df.shape[0]
+N = paavo_sub.shape[0]
 x = drowning_data[:,0]
 y = drowning_data[:,1]
 x_pred = 2019
