4065f48a09609e6b96c6e1c56529cbd0196ea52c
Markku Luotamo (Reaktor) <markku.luotamo@reaktor.fi>
2018-12-09 16:01:43 +0200

    revert postal_region_ix lb

--- model/hierarchical_single_var_logitbin.py
+++ model/hierarchical_single_var_logitbin.py
@@ -7,7 +7,7 @@ stan_code = """
 data {
     int<lower=0> n_postal_codes; // number of postal code data points
     int <lower=0> n_postal_regions; // number of two-digit areas (groups)
-    int<lower=0,upper=n_postal_regions> postal_region_ix[n_postal_codes]; // group indicator
+    int<lower=1,upper=n_postal_regions> postal_region_ix[n_postal_codes]; // group indicator
     int n_affluent[n_postal_codes]; // observations
     int n_households[n_postal_codes]; // total number of households per postal code
 }
